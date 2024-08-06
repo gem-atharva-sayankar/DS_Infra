6f2cf5294c36322c2f2f50944476366d0e21ea4e
gem-atharva-sayankar <151812675+gem-atharva-sayankar@users.noreply.github.com>
2024-08-06 13:46:28 +0530

    Update outputs.tf

--- Infrastructure/Tata_AIG/outputs.tf
+++ Infrastructure/Tata_AIG/outputs.tf
@@ -1,8 +1,3 @@
 
 
 
-
-output "swagger_endpoint" {
-  value       = "${module.alb_server.dns_alb}/api/docs"
-  description = "Copy this value in your browser in order to access the swagger documentation"
-}
